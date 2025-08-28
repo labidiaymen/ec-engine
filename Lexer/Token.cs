10a5e1256551878e94d6e5fbac1e570f6543af72
Aymen <labidi@aymen.co>
2025-08-28 03:19:00 +0100

    first commit

---
+++ Lexer/Token.cs
@@ -0,0 +1,15 @@
+namespace ECEngine.Lexer;
+
+// Enum for all token types
+public enum Token
+{
+    Identifier,
+    Number,
+    Operator,
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Semicolon,
+    EOF
+}
